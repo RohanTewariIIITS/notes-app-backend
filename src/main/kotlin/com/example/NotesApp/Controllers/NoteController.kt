f39b4038b71822728ebae945f798f52082a94373
Rohan Tewari <rohantewari2023@gmail.com>
2025-11-25 22:33:48 +0530

    ammendment to backend - implemented delete facility

--- src/main/kotlin/com/example/NotesApp/Controllers/NoteController.kt
+++ src/main/kotlin/com/example/NotesApp/Controllers/NoteController.kt
@@ -2,6 +2,7 @@ package com.example.NotesApp.Controllers
 
 import com.example.NotesApp.Models.Note
 import com.example.NotesApp.Repositories.NoteRepository
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -26,5 +27,10 @@ class NoteController(
     @PutMapping("/update/{id}")
     fun updateNote(@PathVariable id: String, @RequestBody updatedNote: Note){
         repository.save(updatedNote)
+    }
+
+    @DeleteMapping("/remove/{id}")
+    fun removeNote(@PathVariable id: String){
+        repository.deleteById(id)
     }
 }
